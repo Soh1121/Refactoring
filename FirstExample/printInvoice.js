4496e42be1eae1c8259b1d1f0bc9c4279b3341c2
Soh1121 <satou.shg@gmail.com>
2023-10-19 11:43:33 +0900

    [refactor] 変数名を変更

--- FirstExample/printInvoice.js
+++ FirstExample/printInvoice.js
@@ -1,24 +1,24 @@
 function statement (invoice, plays) {
     function amountFor(pref, play) {
-        let thisAmount = 0;
+        let result = 0;
         switch (play.type) {
         case "tragedy":
-            thisAmount = 40000;
+            result = 40000;
             if (pref.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+                result += 1000 * (perf.audience - 30);
             }
             break;
         case "comedy":
-            thisAmount = 30000;
+            result = 30000;
             if (pref.audience > 20) {
-                thisAmount += 10000 + 500 * (pref.audience - 20);
+                result += 10000 + 500 * (pref.audience - 20);
             }
-            thisAmount += 300 * pref.audience;
+            result += 300 * pref.audience;
             break;
         default:
             throw new Error(`unknown type: ${play.type}`);
         }
-        return thisAmount;
+        return result;
     }
 
     let totalAmount = 0;

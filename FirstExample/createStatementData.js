fe1f5c3ab430ba324004c1221f585de1f0fc4cc4
Soh1121 <satou.shg@gmail.com>
2023-10-29 09:18:22 +0900

    [refactor] 【関数の移動】ボリューム特典ポイントの計算処理の移動

--- FirstExample/createStatementData.js
+++ FirstExample/createStatementData.js
@@ -26,10 +26,7 @@ function createStatementData(invoice, plays) {
     }
 
     function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
     }
 
     function playFor(aPerformance) {
@@ -62,6 +59,13 @@ class PerformanceCalculator {
             default:
                 throw new Error(`unknown type: ${this.play.type}`);
         }
+        return result;
+    }
+
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.performance.play.type) result += Math.floor(this.performance.audience / 5);
         return result;
     }
 }

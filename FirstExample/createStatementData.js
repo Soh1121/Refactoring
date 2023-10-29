d1e2146bb17f3d2daa22e6e8dee3ef4a1745403f
Soh1121 <satou.shg@gmail.com>
2023-10-29 09:31:56 +0900

    [refactor] 適切なサブクラスのインスタンスを生成して返す

--- FirstExample/createStatementData.js
+++ FirstExample/createStatementData.js
@@ -67,5 +67,16 @@ class PerformanceCalculator {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch(aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+    default:
+        throw new Error(`未知の演劇の種類: ${aPlay.type}`);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+}
+
+class ComedyCalculator extends PerformanceCalculator {
 }

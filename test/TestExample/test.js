253f318ff28705d4d90ec0f18f1416111c34ff86
Soh1121 <satou.shg@gmail.com>
2023-11-10 11:36:47 +0900

    [refactor] 共通部分を抽出

--- test/TestExample/test.js
+++ test/TestExample/test.js
@@ -3,12 +3,14 @@ const expect = chai.expect;
 const { Province, sampleProvinceData } = require('../../TestExample/sample');
 
 describe('province', function() {
+    let asia;
+    beforeEach(function() {
+        asia = new Province(sampleProvinceData());
+    });
     it('shortfall', () => {
-        const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     });
     it('profit', () => {
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).equal(230);
     });
 });

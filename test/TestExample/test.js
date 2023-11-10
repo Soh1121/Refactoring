1df4c7a31ae6a9bd9204c74b64feccc16b8e2e5a
Soh1121 <satou.shg@gmail.com>
2023-11-10 11:56:16 +0900

    [update] 境界値のテストを追加

--- test/TestExample/test.js
+++ test/TestExample/test.js
@@ -18,4 +18,51 @@ describe('province', function() {
         expect(asia.shortfall).equal(-6);
         expect(asia.profit).equal(292);
     });
+    it('zero demand', () => {
+        asia.demand = 0;
+        expect(asia.shortfall).equal(-25);
+        expect(asia.profit).equal(0);
+    });
+    it('negative demand', () => {
+       asia.demand = -1;
+       expect(asia.shortfall).equal(-26);
+       expect(asia.profit).equal(-10);
+    });
+    it('empty string demand', () => {
+        asia.demand = "";
+        expect(asia.shortfall).NaN;
+        expect(asia.profit).NaN;
+    });
+});
+
+describe('no producers', function() {
+    let noProducers;
+    beforeEach(function () {
+       const data = {
+           name: "No producers",
+           producers: [],
+           demand: 30,
+           price: 20
+       };
+       noProducers = new Province(data);
+    });
+    it('shortfall', () => {
+        expect(noProducers.shortfall).equal(30);
+    });
+    it('profit', () => {
+        expect(noProducers.profit).equal(0);
+    });
+});
+
+describe('string for producers', function() {
+    it('', () => {
+        const data = {
+            name: "String producers",
+            producers: "",
+            demand: 30,
+            price: 20,
+        };
+        const prov = new Province(data);
+        expect(prov.shortfall).equal(0);
+    });
 });

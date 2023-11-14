fa89abed2d1aa6a866797dd5c7629fbf880364aa
Soh1121 <satou.shg@gmail.com>
2023-11-14 16:01:56 +0900

    [refactor] 元のクラスを削除

--- Encapsulation/InlineClass.js
+++ Encapsulation/InlineClass.js
@@ -1,13 +1,3 @@
-class TrackingInformation {
-    get shippingCompany() {return this._shippingCompany;}
-    set shippingCompany(arg) {this._shippingCompany = arg;}
-    get trackingNumber() {return this._trackingNumber;}
-    set trackingNumber(arg) {this._trackingNumber = arg;}
-    get display() {
-        return `${this.shippingCompany}: ${this.trackingNumber}`;
-    }
-}
-
 class Shipment {
     get shippingCompany() {return this._shippingCompany;}
     set shippingCompany(arg) {this._shippingCompany = arg;}

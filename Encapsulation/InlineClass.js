8850fd6dddf95653ae26b4021138b5e875de6f7d
Soh1121 <satou.shg@gmail.com>
2023-11-14 16:01:40 +0900

    [refactor] すべての関数とデータをインライン先に移動

--- Encapsulation/InlineClass.js
+++ Encapsulation/InlineClass.js
@@ -9,16 +9,12 @@ class TrackingInformation {
 }
 
 class Shipment {
-    get shippingCompany() {return this._trackingInformation.shippingCompany;}
-    set shippingCompany(arg) {this._trackingInformation.shippingCompany = arg;}
-    get trackingNumber() {return this._trackingInformation.trackingNumber;}
-    set trackingNumber(arg) {this._trackingInformation.trackingNumber = arg;}
+    get shippingCompany() {return this._shippingCompany;}
+    set shippingCompany(arg) {this._shippingCompany = arg;}
+    get trackingNumber() {return this._trackingNumber;}
+    set trackingNumber(arg) {this._trackingNumber = arg;}
     get trackingInfo() {
-        return this._trackingInformation.display;
-    }
-    get trackingInformation() {return this._trackingInformation;}
-    set trackingInformation(aTrackingInformation) {
-        this._trackingInformation = aTrackingInformation;
+        return `${this.shippingCompany}: ${this.trackingNumber}`;
     }
 }
 

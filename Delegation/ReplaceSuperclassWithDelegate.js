d9dfd84d6adbba02ef5707cfa3ee71eb1d3a02b6
Soh1121 <satou.shg@gmail.com>
2023-11-15 09:37:54 +0900

    [update] ロード用のルーチンを作成

--- Delegation/ReplaceSuperclassWithDelegate.js
+++ Delegation/ReplaceSuperclassWithDelegate.js
@@ -30,3 +30,12 @@ class Scroll {
         return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
     }
 }
+
+// load routine
+const scrolls = aDocument
+    .map(record => new Scroll(
+        record.id,
+        record.catalogData.title,
+        record.catalogData.tags,
+        LocalDate.parse(record.lastCleaned)
+    ));

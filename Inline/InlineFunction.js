3c1867e857a04eff655c10132ba8bf532612528a
Soh1121 <satou.shg@gmail.com>
2023-11-15 13:52:19 +0900

    [refactor] 【ステートメントの呼び出し側への移動】残りの行のインライン化

--- Inline/InlineFunction.js
+++ Inline/InlineFunction.js
@@ -1,10 +1,9 @@
 function reportLines(aCustomer) {
     const lines = [];
     lines.push(["name", aCustomer.name]);
-    gatherCustomerData(lines, aCustomer);
+    lines.push(["location", aCustomer.location]);
     return lines;
 }
 
 function gatherCustomerData(out, aCustomer) {
-    out.push(["location", aCustomer.location]);
 }

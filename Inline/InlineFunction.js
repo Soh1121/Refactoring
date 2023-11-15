b9196960f9910dafccdf85b7f01ed74dcc1405e7
Soh1121 <satou.shg@gmail.com>
2023-11-15 09:53:06 +0900

    [refactor] 関数を削除

--- Inline/InlineFunction.js
+++ Inline/InlineFunction.js
@@ -1,7 +1,3 @@
 function rating(aDriver) {
     return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
-
-function moreThanFiveLateDeliveries(aDriver) {
-    return aDriver.numberOfLateDeliveries > 5;
-}

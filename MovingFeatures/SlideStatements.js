da4a6db95dade2fccdb129392cf15bf6feaa0bd5
Soh1121 <satou.shg@gmail.com>
2023-11-17 09:32:26 +0900

    [add] サンプルコードを作成

---
+++ MovingFeatures/SlideStatements.js
@@ -0,0 +1,13 @@
+const pricingPlan = retrievePricingPlan();
+const order = retrieveOrder();
+const basecharge = pricingPlan.base;
+let charge;
+const chargePerUnit = pricingPlan.unit;
+const units = order.units;
+let discount;
+charge = basecharge + units * chargePerUnit;
+let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
+discount = discountableUnits * pricingPlan.discountFactor;
+if (order.isRepeat) discount += 20;
+charge = charge - discount;
+chargeOrder(charge);

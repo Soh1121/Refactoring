db691397dc046d4aacaeaa83c862cc2f7883947c
Soh1121 <satou.shg@gmail.com>
2023-11-21 11:30:39 +0900

    [update] サンプルコードを変更

--- Variables/SplitVariable.js
+++ Variables/SplitVariable.js
@@ -1,13 +1,5 @@
-function distanceTravelled (scenario, time) {
-    let result;
-    const primaryAcceleration = scenario.primaryForce / scenario.mass;
-    let primaryTime = Math.min(time, scenario.delay);
-    result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
-    let secondaryTime = time - scenario.delay;
-    if (secondaryTime > 0) {
-        let primaryVelocity = primaryAcceleration * scenario.delay;
-        const secondaryAcceleration = (senario.primaryForce + senario.secondaryForce) / senario.mass;
-        result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
-    }
-    return result;
+function discount (inputValue, quantity) {
+    if (inputValue > 50) inputValue = inputValue - 2;
+    if (quantity > 100) inputValue = inputValue - 1;
+    return inputValue;
 }

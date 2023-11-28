a84a5fa3a1c86163998570c38bb8ea1f81417ff8
Soh1121 <satou.shg@gmail.com>
2023-11-28 09:49:11 +0900

    [refactor] 戻り値の名前を普段のコーディングスタイルに合わせる

--- Basic/ExtractFunction.js
+++ Basic/ExtractFunction.js
@@ -3,17 +3,17 @@ function printOwing(invoice) {
     printBanner();
 
     // 未払い金の計算(calculate outstanding)
-    let outstanding = calculateOutstanding(invoice);
+    const outstanding = calculateOutstanding(invoice);
     recordDueDate(invoice);
     printDetails(invoice, outstanding);
 }
 
 function calculateOutstanding(invoice) {
-    let outstanding = 0;
+    let result = 0;
     for (const o of invoice.orders) {
-        outstanding += o.amount;
+        result += o.amount;
     }
-    return outstanding;
+    return result;
 }
 
 function recordDueDate(invoice) {

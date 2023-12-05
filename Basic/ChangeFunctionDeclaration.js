f47ab798c97c30e419de58a79af261d3741f7f4f
Soh1121 <satou.shg@gmail.com>
2023-12-05 21:06:13 +0900

    [update] サンプルコードを変更

--- Basic/ChangeFunctionDeclaration.js
+++ Basic/ChangeFunctionDeclaration.js
@@ -1,11 +1,5 @@
-class Book
-{
-    addReservation(customer) {
-        this.zz_addReservation(customer, false);
-    }
-
-    zz_addReservation(customer, isPriority) {
-        assert(isPriority === true || isPriority === false);
-        this._reservations.push(customer, isPriority);
-    }
+function inNewEngland(aCustomer) {
+    return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
 }
+
+const newEnglanders = someCustomers.filter(c => inNewEngland(c));

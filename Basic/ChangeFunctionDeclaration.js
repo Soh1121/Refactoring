67d27b076f8ca7881d2c45d5de8deaf6774945e7
Soh1121 <satou.shg@gmail.com>
2023-12-05 21:04:25 +0900

    [refactor] 【アサーションの導入】新たなパラメータが呼び出し側で設定されているか確認

--- Basic/ChangeFunctionDeclaration.js
+++ Basic/ChangeFunctionDeclaration.js
@@ -5,6 +5,7 @@ class Book
     }
 
     zz_addReservation(customer, isPriority) {
+        assert(isPriority === true || isPriority === false);
         this._reservations.push(customer, isPriority);
     }
 }

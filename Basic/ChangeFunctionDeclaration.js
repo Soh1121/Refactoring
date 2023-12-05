a4058fb92651dcf8a1ea9f8c2911df0e05e10aaf
Soh1121 <satou.shg@gmail.com>
2023-12-05 21:01:43 +0900

    [update] サンプルコードを変更

--- Basic/ChangeFunctionDeclaration.js
+++ Basic/ChangeFunctionDeclaration.js
@@ -1,7 +1,6 @@
-function circum(radius) {
-    return circumference(radius);
-}
-
-function circumference(radius) {
-    return 2 * Math.PI * radius;
+class Book
+{
+    addReservation(customer) {
+        this._reservations.push(customer);
+    }
 }

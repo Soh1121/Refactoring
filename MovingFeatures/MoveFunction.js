e2bfd00bcc6662a33f793047d08456fb11f1c62b
Soh1121 <satou.shg@gmail.com>
2023-12-11 10:43:02 +0900

    [update] サンプルコードを変更

--- MovingFeatures/MoveFunction.js
+++ MovingFeatures/MoveFunction.js
@@ -1,35 +1,20 @@
-function trackSummary(points) {
-    const totalTime = calculateTime();
-    const pace = totalTime / 60 / totalDistance(points);
-    return {
-        time: totalTime,
-        distance: totalDistance(points),
-        pace: pace
-    };
-
-    function calculateTime() { ... }
-}
-
-function totalDistance(points) {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-        result += distance(points[i - 1], points[i]);
+class Account
+{
+    get bankCharge() {
+        let result = 4.5;
+        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+        return result;
     }
-    return result;
-}
 
-function distance(p1, p2) {
-    const EARTH_RADIUS = 3959;
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a = Math.pow(Math.sin(dLat / 2), 2)
-        + Math.cos(radians(p2.lat))
-        * Math.cos(radians(p1.lat))
-        * Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return EARTH_RADIUS * c;
-}
-
-function radians(degrees) {
-    return degrees * Math.PI / 180;
+    get overdraftCharge() {
+        if (this.type.isPremium) {
+            const baseCharge = 10;
+            if (this.daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+        }
+        else
+            return this.daysOverdrawn * 1.75;
+    }
 }

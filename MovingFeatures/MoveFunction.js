fa9c9ccc0000575110780edb7dc5d88492c5ca9c
Soh1121 <satou.shg@gmail.com>
2023-12-11 10:48:42 +0900

    [refactor] 元のメソッド本体を委譲呼び出しに置き換え

--- MovingFeatures/MoveFunction.js
+++ MovingFeatures/MoveFunction.js
@@ -7,15 +7,7 @@ class Account
     }
 
     get overdraftCharge() {
-        if (this.type.isPremium) {
-            const baseCharge = 10;
-            if (this.daysOverdrawn <= 7)
-                return baseCharge;
-            else
-                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-        }
-        else
-            return this.daysOverdrawn * 1.75;
+        this.type.overdraftCharge(this.daysOverdrawn);
     }
 }
 

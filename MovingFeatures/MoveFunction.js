9901b5565475ce9d0f8db82dba18dbc590da3501
Soh1121 <satou.shg@gmail.com>
2023-12-11 10:47:15 +0900

    [refactor] 対象メソッドをコピー

--- MovingFeatures/MoveFunction.js
+++ MovingFeatures/MoveFunction.js
@@ -18,3 +18,18 @@ class Account
             return this.daysOverdrawn * 1.75;
     }
 }
+
+class AccountType
+{
+    overdraftCharge(daysOverdrawn) {
+        if (this.isPremium) {
+            const baseCharge = 10;
+            if (daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+        }
+        else
+            return daysOverdrawn * 1.75;
+    }
+}

ee1281b517acd2258237ac5763fb5422365f5bad
Soh1121 <satou.shg@gmail.com>
2024-01-08 15:23:35 +0900

    [refactor] 付加されたフィールドを使うように変更

--- Basic/CombineFunctionsIntoTransform.js
+++ Basic/CombineFunctionsIntoTransform.js
@@ -12,15 +12,15 @@ function client2() {
 function client3() {
     const rawReading = acquireReading();
     const aReading = enrichReading(rawReading);
-    const basicChargeAmount = calculateBaseCharge(aReading);
-
-    function calculateBaseCharge(aReading) {
-        return baseRate(aReading.month, aReading.year) * aReading.quantity;
-    }
+    const basicChargeAmount = aReading.baseCharge;
 }
 
 function enrichReading(original) {
     const result = _.cloneDeep(original);
     result.baseCharge = calculateBaseCharge(result);
     return result;
+}
+
+function calculateBaseCharge(aReading) {
+    return baseRate(aReading.month, aReading.year) * aReading.quantity;
 }

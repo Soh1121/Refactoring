e654f7628323840dc83ef49589c1db05befa2d0f
Soh1121 <satou.shg@gmail.com>
2024-01-08 15:33:24 +0900

    [refactor] 関数を変換関数に移動

--- Basic/CombineFunctionsIntoTransform.js
+++ Basic/CombineFunctionsIntoTransform.js
@@ -19,6 +19,7 @@ function client3() {
 function enrichReading(original) {
     const result = _.cloneDeep(original);
     result.baseCharge = calculateBaseCharge(result);
+    result.taxableCharge = Math.max(0, result.baseCharge - taxThreshold(result.year));
     return result;
 }
 

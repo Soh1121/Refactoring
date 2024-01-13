e7991c5c6cab138037d73cae0e6bbba24196bb8e
Soh1121 <satou.shg@gmail.com>
2024-01-13 17:11:26 +0900

    [refactor] 関数名から接頭辞を除去

--- RefactoringApis/ReplaceQueryWithParameter.js
+++ RefactoringApis/ReplaceQueryWithParameter.js
@@ -1,15 +1,11 @@
 class HeatingPlan {
-    get targetTemperature() {
-        return this.xxNEWTargetTemperature(thermostat.selectedTemperature);
-    }
-
-    xxNEWTargetTemperature(selectedTemperature) {
+    targetTemperature(selectedTemperature) {
         if      (selectedTemperature > this._max) return this._max;
         else if (selectedTemperature < this._min) return this._min;
         else return selectedTemperature;
     }
 }
 
-if      (thePlan.xxNEWTargetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature) setToHeat();
-else if (thePlan.xxNEWTargetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature) setToCool();
+if      (thePlan.targetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature) setToHeat();
+else if (thePlan.targetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature) setToCool();
 else setOff();

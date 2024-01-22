8a7e9591eced6249230e6216ab3705e6cea5b56a
Soh1121 <satou.shg@gmail.com>
2024-01-22 21:49:14 +0900

    [refactor]【関数の抽出】ループを関数化

--- MovingFeatures/SplitLoop.js
+++ MovingFeatures/SplitLoop.js
@@ -1,11 +1,18 @@
-let totalSalary = 0;
-for (const p of people) {
-    totalSalary += p.salary;
+return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+
+function totalSalary() {
+    let totalSalary = 0;
+    for (const p of people) {
+        totalSalary += p.salary;
+    }
+    return totalSalary;
 }
 
-let youngest = people[0] ? people[0].age : Infinity;
-for (const p of people) {
-    if (p.age < youngest) youngest = p.age;
-}
 
-return `youngestAge: ${youngest}, totalSalary: ${totalSalary}`;
+function youngestAge() {
+    let youngest = people[0] ? people[0].age : Infinity;
+    for (const p of people) {
+        if (p.age < youngest) youngest = p.age;
+    }
+    return youngest;
+}

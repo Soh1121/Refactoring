3c2b24194cd8cf7f097032e2fae93d160b4bfddb
Soh1121 <satou.shg@gmail.com>
2024-01-28 16:32:08 +0900

    [update] サンプルコードを変更

--- SimplifyConditionalLogic/ReplaceConditionalWithPolymorphism.js
+++ SimplifyConditionalLogic/ReplaceConditionalWithPolymorphism.js
@@ -1,64 +1,54 @@
-function plumage(bird) {
-    return createBird(bird).plumage;
+function rating(voyage, history) {
+    const vpf = voyageProfitFactor(voyage, history);
+    const vr = voyageRsisk(voyage);
+    const chr = captainHistoryRisk(voyage, history);
+    if (vpf * 3 > (vr + chr * 2)) return "A";
+    else return "B";
 }
 
-function speeds(bird) {
-    return createBird(bird).airSpeedVelocity;
+function voyageRsisk(voyage) {
+    let result = 1;
+    if (voyage.length > 4) result += 2;
+    if (voyage.length > 8) result += voyage.length - 8;
+    if (["china", "east-indies"].includes(voyage.zone)) result += 4;
+    return Math.max(result, 0);
 }
 
-function createBird(bird) {
-    switch (bird.type) {
-    case 'EuropeanSwallow':
-        return new EuropeanSwallow(bird);
-    case 'AfricanSwallow':
-        return new AfricanSwallow(bird);
-    case 'NorwegianBlueParrot':
-        return new NorwegianBlueParrot(bird);
-    default:
-        return new Bird(bird);
-    }
+function captionHistoryRisk(voyage, history) {
+    let result = 1;
+    if (history.length < 5) result += 4;
+    result += history.filter(v => v.profit < 0).length;
+    if (voyage.zone === "china" && hasChina(history)) result -= 2;
+    return Math.max(result, 0);
 }
 
-class EuropeanSwallow extends Bird {
-    get plumage() {
-        return "average";
-    }
-
-    get airSpeedVelocity() {
-        return 35;
-    }
+function hasChina(history) {
+    return history.some(v => "china" === v.zone);
 }
 
-class AfricanSwallow extends Bird {
-    get plumage() {
-        return (this.numberOfCoconuts > 2) ? "tired" : "average";
+function voyageProfitFactor(voyage, history) {
+    let result = 2;
+    if (voyage.zone === "china") result += 1;
+    if (voyage.zone === "east-indies") result += 1;
+    if (voyage.zone === "china" && hasChina(history)) {
+        result += 3;
+        if (history.length > 10) result += 1;
+        if (voyage.length > 12) result += 1;
+        if (voyage.length > 18) result -= 1;
     }
-
-    get airSpeedVelocity() {
-        return 40 - 2 * this.numberOfCoconuts;
+    else {
+        if (history.length > 8) result += 1;
+        if (voyage.length > 14) result -= 1;
     }
+    return result;
 }
 
-class NorwegianBlueParrot extends Bird {
-    get plumage() {
-        return (this.voltage > 100) ? "scorched" : "beautiful";
-    }
+const voyage = {zone: "west-indies", length: 10};
+const history = [
+    {zone: "east-indies", profit:  5},
+    {zone: "west-indies", profit: 15},
+    {zone: "china",       profit: -2},
+    {zone: "west-africa", profit:  7},
+];
 
-    get airSpeedVelocity() {
-        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
-    }
-}
-
-class Bird {
-    constructor(birdObject) {
-        Object.assign(this, birdObject);
-    }
-
-    get plumage() {
-            return "unknown";
-    }
-
-    get airSpeedVelocity() {
-        return null;
-    }
-}
+const myRating = rating(voyage, history);

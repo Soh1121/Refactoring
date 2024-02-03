904335e89cded98aecbd42c801be1d0e5b904f46
Soh1121 <satou.shg@gmail.com>
2024-02-03 17:06:11 +0900

    [refactor]【関数の移動】コンテキストを適切に合わせる

--- RefactoringApis/PreserveWholeObject.js
+++ RefactoringApis/PreserveWholeObject.js
@@ -1,5 +1,5 @@
 const tempRange = aRoom.daysTempRange;
-const withinRange = xxNEWwithinRange(aPlan, tempRange);
+const withinRange = aPlan.xxNEWwithinRange(tempRange);
 if (!isWithinRange)
     alerts.push("室温が設定値を超えました");
 
@@ -8,11 +8,11 @@ class HeatingPlan
     withinRange(bottom, top) {
         return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
     }
-}
 
-function xxNEWwithinRange(aPlan, tempRange) {
-    const low = tempRange.low;
-    const high = tempRange.high;
-    const isWithinRange = aPlan.withinRange(low, high);
-    return isWithinRange;
+    xxNEWwithinRange(tempRange) {
+        const low = tempRange.low;
+        const high = tempRange.high;
+        const isWithinRange = this.withinRange(low, high);
+        return isWithinRange;
+    }
 }

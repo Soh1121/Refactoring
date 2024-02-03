79ce07137446e3cf8a6c48df767a8bba488db0a9
Soh1121 <satou.shg@gmail.com>
2024-02-03 16:51:37 +0900

    [add] サンプルコードを作成

---
+++ RefactoringApis/PreserveWholeObject.js
@@ -0,0 +1,11 @@
+const low = aRoom.daysTempRange.low;
+const high = aRoom.daysTempRange.high;
+if (!AnimationPlaybackEvent.withinRange(low, high))
+    alerts.push("室温が設定値を超えました");
+
+class HeatingPlan
+{
+    withinRange(bottom, top) {
+        return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
+    }
+}

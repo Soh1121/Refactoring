7032e316ba4fb1dedaac64cfb13da8c1142b38f6
Soh1121 <satou.shg@gmail.com>
2024-02-03 16:55:22 +0900

    [refactor] 既存の関数の呼び出しを新しい関数の呼び出しに置き換え

--- RefactoringApis/PreserveWholeObject.js
+++ RefactoringApis/PreserveWholeObject.js
@@ -1,6 +1,6 @@
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
-if (!AnimationPlaybackEvent.withinRange(low, high))
+if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
     alerts.push("室温が設定値を超えました");
 
 class HeatingPlan

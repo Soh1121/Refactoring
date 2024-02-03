892aa2d4f7bff2e2c0888900a448bc3aa5789644
Soh1121 <satou.shg@gmail.com>
2024-02-03 16:59:14 +0900

    [refactor] 新しい関数呼び出しに変更

--- RefactoringApis/PreserveWholeObject.js
+++ RefactoringApis/PreserveWholeObject.js
@@ -1,13 +1,9 @@
-if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
+if (!aPlan.withinRange(aRoom.daysTempRange))
     alerts.push("室温が設定値を超えました");
 
 class HeatingPlan
 {
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
-    }
-
-    xxNEWwithinRange(aNumberRange) {
+    withinRange(aNumberRange) {
         return (aNumberRange.low >= this._temperatureRange.low) &&
             (aNumberRange.high <= this._temperatureRange.high);
     }

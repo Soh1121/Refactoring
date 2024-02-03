87b504da95c3c11b337aeb580cc5e90a9c923331
Soh1121 <satou.shg@gmail.com>
2024-02-03 17:10:43 +0900

    [refactor] メソッド名の変更とインライン化

--- RefactoringApis/PreserveWholeObject.js
+++ RefactoringApis/PreserveWholeObject.js
@@ -1,15 +1,9 @@
-const tempRange = aRoom.daysTempRange;
-const withinRange = aPlan.xxNEWwithinRange(tempRange);
-if (!isWithinRange)
+if (!aPlan.withinRange(aRoom.daysTempRange))
     alerts.push("室温が設定値を超えました");
 
 class HeatingPlan
 {
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
-    }
-
-    xxNEWwithinRange(tempRange) {
+    withinRange(tempRange) {
         return (tempRange.low >= this._temperatureRange.low) &&
             (tempRange.high <= this._temperatureRange.high);
     }

04008b7cb5fe356031d17e64160258f23df76ecc
Soh1121 <satou.shg@gmail.com>
2024-02-06 14:13:35 +0900

    [refactor] 委譲先となるクラスを新しく作成

--- DealingWithInheritance/ReplaceSubclassWithDelegate.js
+++ DealingWithInheritance/ReplaceSubclassWithDelegate.js
@@ -36,6 +36,14 @@ class PremiumBooking extends Booking
     }
 }
 
+class PremiumBookingDelegate
+{
+    constructor(hostBooking, extras) {
+        this._host = hostBooking;
+        this._extras = extras;
+    }
+}
+
 function createBooking(show, date) {
     return new Booking(show, date);
 }

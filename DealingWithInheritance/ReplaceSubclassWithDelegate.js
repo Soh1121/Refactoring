b8fad0105d31845222b0913554543d745187d6ab
Soh1121 <satou.shg@gmail.com>
2024-02-11 21:07:45 +0900

    [update] サンプルコードを変更

--- DealingWithInheritance/ReplaceSubclassWithDelegate.js
+++ DealingWithInheritance/ReplaceSubclassWithDelegate.js
@@ -1,71 +1,59 @@
-class Booking
-{
-    constructor(show, date) {
-        this._show = show;
-        this._date = date;
-    }
-
-    get hasTalkback() {
-        return (this._premiumDelegate)
-            ? this._premiumDelegate.hasTalkback
-            : this._show.hasOwnProperty('talkback') && !this.isPeakDay;
-    }
-
-    get basePrice() {
-        return (this._premiumDelegate)
-            ? this._premiumDelegate.basePrice
-            : this._privateBasePrice;
-    }
-
-    get _privateBasePrice() {
-        let result = this._show.price;
-        if (this.isPeakDay) result += Math.round(result * 0.15);
-        return result;
-    }
-
-    get hasDinner() {
-        return (this._premiumDelegate)
-            ? this._premiumDelegate.hasDinner
-            : undefined;
-    }
-
-    _bePremium(extras) {
-        this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+function createBird(data) {
+    switch (data.type) {
+        case 'EuropeanSwallow':
+            return new EuropeanSwallow(data);
+        case 'AfricanSwallow':
+            return new AfricanSwallow(data);
+        case 'NorweigianBlueParrot':
+            return new NorwegianBlueParrot(data);
+        default:
+            return new Bird(data);
     }
 }
 
-class PremiumBookingDelegate
-{
-    constructor(hostBooking, extras) {
-        this._host = hostBooking;
-        this._extras = extras;
+class Bird {
+    constructor(data) {
+        this._name = data.name;
+        this._plumage = data.plumage;
     }
 
-    get hasTalkback() {
-        return this._host._show.hasOwnProperty('talkback');
+    get name() {return this._name;}
+
+    get plumage() {
+        return this._plumage || "average";
     }
 
-    get basePrice() {
-        return Math.round(this._host.privateBasePrice + this._extras.premiumFee);
+    get airSpeedVelocity() {return null;}
+}
+
+class EuropeanSwallow extends Bird {
+    get airSpeedVelocity() {return 35;}
+}
+
+class AfricanSwallow extends Bird {
+    constructor(data) {
+        super (data);
+        this._numberOfCoconuts = data.numberOfCoconuts;
     }
 
-    get hasDinner() {
-        return this._extras.hasOwnProperty('dinner') && !this._host.isPeakDay;
+    get airSpeedVelocity() {
+        return 40 - 2 * this._numberOfCoconuts;
     }
 }
 
-function createBooking(show, date) {
-    return new Booking(show, date);
+class NorwegianBlueParrot extends Bird {
+    constructor(data) {
+        super (data);
+        this._voltage = data.voltage;
+        this._isNailed = data.isNailed;
+    }
+
+    get plumage() {
+        if (this._voltage > 100) return "scorched";
+        else return this._plumage || "beautiful";
+    }
+
+    get airSpeedVelocity() {
+        return (this._isNailed) ? 0 : 10 + this._voltage / 10;
+    }
 }
-
-function createPremiumBooking(show, date, extras) {
-    const result = new Booking(show, date, extras);
-    result._bePremium(extras);
-    return result;
-}
-
-// booking client
-aBooking = createBooking(show, date);
-
-// premium client
-aPremiumBooking = createPremiumBooking(show, date, extras);

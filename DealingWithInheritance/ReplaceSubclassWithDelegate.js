9ff5d8563e3083f9c4c63cdce5f1d1770dcf0e46
Soh1121 <satou.shg@gmail.com>
2024-02-12 21:31:05 +0900

    [refactor]【関数の移動】airSpeedVelocityメソッドを移動

--- DealingWithInheritance/ReplaceSubclassWithDelegate.js
+++ DealingWithInheritance/ReplaceSubclassWithDelegate.js
@@ -44,7 +44,7 @@ class AfricanSwallow extends Bird {
     }
 
     get airSpeedVelocity() {
-        return 40 - 2 * this._numberOfCoconuts;
+        return this._speciesDelegate.airSpeedVelocity;
     }
 }
 
@@ -68,6 +68,10 @@ class NorwegianBlueParrot extends Bird {
 class AfricanSwallowDelegate {
     constructor(data) {
         this._numberOfCoconuts = data.numberOfCoconuts;
+    }
+
+    get airSpeedVelocity() {
+        return 40 - 2 * this._numberOfCoconuts;
     }
 }
 

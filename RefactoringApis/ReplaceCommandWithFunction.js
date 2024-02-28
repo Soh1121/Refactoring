04c51c2d54b1b3ba31f90eda1db26e19bc318aa8
Soh1121 <satou.shg@gmail.com>
2024-02-28 21:46:06 +0900

    [refactor]【デッドコードの削除】使用しなくなったコマンドを削除

--- RefactoringApis/ReplaceCommandWithFunction.js
+++ RefactoringApis/ReplaceCommandWithFunction.js
@@ -1,18 +1,6 @@
-class ChargeCalculator {
-    constructor (customer, usage, provider) {
-    }
-    get baseCharge() {
-        return this._customer.baseRate * this._usage;
-    }
-    charge(customer, usage, provider) {
-        const baseCharge = customer.baseRate * usage;
-        return baseCharge + provider. connectionCharge;
-    }
-}
-
 monthCharge = charge(customer, usage, provider);
 
 function charge(customer, usage, provider) {
     const baseCharge = customer.baseRate * usage;
-    return baseCharge + provider. connectionCharge;
+    return baseCharge + provider.connectionCharge;
 }

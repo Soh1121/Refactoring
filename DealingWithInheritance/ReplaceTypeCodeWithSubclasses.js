a564e2fe315c99448fb480781a1e98cda00aaf8a
Soh1121 <satou.shg@gmail.com>
2024-03-08 22:16:24 +0900

    [refactor] タイプコード用のフィールドとスーパークラスの getter を削除

--- DealingWithInheritance/ReplaceTypeCodeWithSubclasses.js
+++ DealingWithInheritance/ReplaceTypeCodeWithSubclasses.js
@@ -2,13 +2,11 @@ class Employee {
     constructor (name, type) {
         this.validateType(type);
         this._name = name;
-        this._type = type;
     }
     validateType(arg) {
         if (!["engineer", "manager", "salesman"].includes(arg))
             throw new Error(`従業員のタイプコードが不正：${arg}`);
     }
-    get type() {return this._type;}
     toString() {return `${this._name} (${this.type})`;}
 }
 

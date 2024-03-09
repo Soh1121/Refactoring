efdbd5323246a3a03fe49c7a60805728fdd94710
Soh1121 <satou.shg@gmail.com>
2024-03-09 17:04:07 +0900

    [refactor] 呼び出し元を変更

--- RefactoringApis/RemoveFlagArgument.js
+++ RefactoringApis/RemoveFlagArgument.js
@@ -1,8 +1,3 @@
-function deliveryDate(anOrder, isRush) {
-    if (isRush) return rushDeliveryDate(anOrder);
-    else return regularDeliveryDate(anOrder);
-}
-
 function rushDeliveryDate(anOrder) {
     let deliveryTime;
     if (["MA", "CT"]     .includes(anOrder.deliveryState)) deliveryTime = 1;
@@ -19,6 +14,6 @@ function regularDeliveryDate(anOrder) {
     return anOrder.placedOn.plusDays(2 + deliveryTime);
 }
 
-aShipment.deliveryDate = deliveryDate(anOrder, true);
+aShipment.deliveryDate = rushDeliveryDate(anOrder);
 
-aShipment.deliveryDate = deliveryDate(anOrder, false);
+aShipment.deliveryDate = regularDeliveryDate(anOrder);

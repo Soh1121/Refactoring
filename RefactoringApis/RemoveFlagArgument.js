686dfc7e1648e7e203ea3982f929e4d0a80e8147
Soh1121 <satou.shg@gmail.com>
2024-03-09 17:09:02 +0900

    [update] サンプルコードを変更

--- RefactoringApis/RemoveFlagArgument.js
+++ RefactoringApis/RemoveFlagArgument.js
@@ -1,19 +1,22 @@
-function rushDeliveryDate(anOrder) {
+function deliveryDate(anOrder, isRush) {
+    let result;
     let deliveryTime;
-    if (["MA", "CT"]     .includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
-    return anOrder.placedOn.plusDays(1 + deliveryTime);
+    if (anOrder.deliveryState === "MA" || anOrder.deliveryState === "CT")
+        deliveryTime = isRush ? 1 : 2;
+    else if (anOrder.deliveryState === "NY" || anOrder.deliveryState === "NH") {
+        deliveryTime = 2;
+        if (anOrder.deliveryState === "NH" && !isRush)
+            deliveryTime = 3;
+    }
+    else if (isRush)
+        deliveryTime = 3;
+    else
+        deliveryTime = 4;
+    result = anOrder.placedOn.plusDays(2 + deliveryTime);
+    if (isRush) result = result.minusDays(1);
+    return result;
 }
 
-function regularDeliveryDate(anOrder) {
-    let deliveryTime;
-    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if (["ME", "NH"] .includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
-    return anOrder.placedOn.plusDays(2 + deliveryTime);
-}
+aShipment.deliveryDate = deliveryDate(anOrder);
 
-aShipment.deliveryDate = rushDeliveryDate(anOrder);
-
-aShipment.deliveryDate = regularDeliveryDate(anOrder);
+aShipment.deliveryDate = deliveryDate(anOrder);

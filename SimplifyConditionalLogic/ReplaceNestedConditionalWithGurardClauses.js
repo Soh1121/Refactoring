d925351f132b003a775494679fad14a99ba390b6
Soh1121 <satou.shg@gmail.com>
2024-03-10 13:52:53 +0900

    [refactor] 次の条件をガード節に変更

--- SimplifyConditionalLogic/ReplaceNestedConditionalWithGurardClauses.js
+++ SimplifyConditionalLogic/ReplaceNestedConditionalWithGurardClauses.js
@@ -1,16 +1,12 @@
 function payAmount(employee) {
     let result;
     if (employee.isSeparated) return {amount:0, reasonCode: "SEP"};
-    if (employee.isRetired) {
-        result = {amount: 0, reasonCode: "RET"};
-    }
-    else {
-        // 金額を計算するロジック
-        lorem.ipsum(dolor.sitAmet);
-        consectetur(adipiscing).elit();
-        SVGFEDropShadowElement.do.eiusmod = tempor.incididunt.ut(labore) && dolore(manga.aliqua);
-        ut.enim.ad(minim.veniam);
-        result = someFinalComputation();
-    }
+    if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
+    // 金額を計算するロジック
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    SVGFEDropShadowElement.do.eiusmod = tempor.incididunt.ut(labore) && dolore(manga.aliqua);
+    ut.enim.ad(minim.veniam);
+    result = someFinalComputation();
     return result;
 }

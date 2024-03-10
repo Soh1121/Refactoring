978e3fcb983c57a81fd16f77a3c5ce9fafd97777
Soh1121 <satou.shg@gmail.com>
2024-03-10 13:55:13 +0900

    [update] サンプルコードを変更

--- SimplifyConditionalLogic/ReplaceNestedConditionalWithGurardClauses.js
+++ SimplifyConditionalLogic/ReplaceNestedConditionalWithGurardClauses.js
@@ -1,10 +1,9 @@
-function payAmount(employee) {
-    if (employee.isSeparated) return {amount:0, reasonCode: "SEP"};
-    if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
-    // 金額を計算するロジック
-    lorem.ipsum(dolor.sitAmet);
-    consectetur(adipiscing).elit();
-    SVGFEDropShadowElement.do.eiusmod = tempor.incididunt.ut(labore) && dolore(manga.aliqua);
-    ut.enim.ad(minim.veniam);
-    return someFinalComputation();
+function adjustedCapital(anInstrument) {
+    let result = 0;
+    if (anInstrument.capital > 0) {
+        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+            result = (anInstrument.income/ anInstrument.duration) * anInstrument.adjustmentFactor;
+        }
+    }
+    return result;
 }

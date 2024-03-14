9e546b23749fd35ba27e77275af9490564040d19
Soh1121 <satou.shg@gmail.com>
2024-03-14 18:21:27 +0900

    [update] サンプルコードを変更

--- DealingWithInheritance/PullUpConstructorBody.js
+++ DealingWithInheritance/PullUpConstructorBody.js
@@ -1,20 +1,25 @@
-class Party {
-    constructor(name) {
-        this._name = name;
+class Employee {
+    constructor (name) {
+
+    }
+
+    get isPrivileged() {
+
+    }
+
+    assignCar() {
+
     }
 }
 
-class Employee extends Party {
-    constructor(name, id, monthlyCost) {
+class Manager extends Employee {
+    constructor (name, grade) {
         super(name);
-        this._id = id;
-        this._monthlyCost = monthlyCost;
+        this._grade = grade;
+        if (this.isPrivileged) this.assignCar; // すべてのサブクラスでこの処理を行っている
     }
-}
 
-class Department extends Party {
-    constructor(name, staff) {
-        super(name);
-        this._staff = staff;
+    get isPrivileged() {
+        return this._grade > 4;
     }
 }

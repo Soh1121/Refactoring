12170977311ee99722532d3fa56cce892c15e22f
Soh1121 <satou.shg@gmail.com>
2024-03-14 18:24:39 +0900

    [refactor]【メソッドの引き上げ】スーパークラスに移動

--- DealingWithInheritance/PullUpConstructorBody.js
+++ DealingWithInheritance/PullUpConstructorBody.js
@@ -10,6 +10,10 @@ class Employee {
     assignCar() {
 
     }
+
+    finishConstruction() {
+        if (this.isPrivileged) this.assignCar; // すべてのサブクラスでこの処理を行っている
+    }
 }
 
 class Manager extends Employee {
@@ -17,10 +21,6 @@ class Manager extends Employee {
         super(name);
         this._grade = grade;
         this.finishConstruction();
-    }
-
-    finishConstruction() {
-        if (this.isPrivileged) this.assignCar; // すべてのサブクラスでこの処理を行っている
     }
 
     get isPrivileged() {

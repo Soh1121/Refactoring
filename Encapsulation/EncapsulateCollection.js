d3570a9cba34da8933c61950ea3dcb8835a84ed2
Soh1121 <satou.shg@gmail.com>
2024-03-19 21:29:23 +0900

    [refactor] 適切なカプセル化を実施

--- Encapsulation/EncapsulateCollection.js
+++ Encapsulation/EncapsulateCollection.js
@@ -7,6 +7,16 @@ class Person {
     get name() {return this._name;}
     get courses() {return this._courses;}
     set courses(aList) {this._courses = aList;}
+
+    addCourse(aCource) {
+        this._courses.push(aCource);
+    }
+
+    removeCourse(aCourse, fnIfAbsent = () => {throw new RangeError();}) {
+        const index = this._courses.indexOf(aCourse);
+        if (index === -1) fnIfAbsent();
+        else this._courses.splice(index, 1);
+    }
 }
 
 class Course {
@@ -27,5 +37,5 @@ const basicCourseNames = readBasicCourseNames(filename);
 aPerson.courses = basicCourseNames.map(name => new Course(name, false));
 
 for (const name of readBasicCourseNames(filename)) {
-    aPerson.courses.push(new Course(name, false));
+    aPerson.addCourse(new Course(name, false));
 }

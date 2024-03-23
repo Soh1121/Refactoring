3628167ce8d59b3c8cc2a80ebb1e0698830e1939
Soh1121 <satou.shg@gmail.com>
2024-03-23 21:57:24 +0900

    [refactor] 値を読んでいる箇所を getter に置き換え

--- Encapsulation/EncapsulateRecord.js
+++ Encapsulation/EncapsulateRecord.js
@@ -2,6 +2,7 @@ class Organization {
     constructor(data) {
         this._data = data;
     }
+    get name() {return this._data.name;}
     set name(aString) {this._data.name = aString;}
 }
 
@@ -10,6 +11,6 @@ function getOrganization() {return organization;}
 
 const organization = new Organization({name: "Acme Gooseberries", country: "GB"});
 
-result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+result += `<h1>${getOrganization().name}</h1>`;
 
 getOrganization().name = newName;

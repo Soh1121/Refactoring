bf9452547f12185a25de9adebac6a42525ff6ad4
Soh1121 <satou.shg@gmail.com>
2024-03-24 22:57:13 +0900

    [refactor] すべての参照箇所を関数として抽出し、CustomerData クラスへ移動

--- Encapsulation/EncapsulateRecord.js
+++ Encapsulation/EncapsulateRecord.js
@@ -27,8 +27,8 @@ getCustomerData().setUsage(customerID, year, month, amount);
 
 // sample read
 function compareUsage (customerID, laterYear, month) {
-    const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
-    const earlier = getRawDataOfCustomers[customerID].usages[laterYear - 1][month];
+    const later = getRawDataOfCustomers().usage(customerID, laterYear, month);
+    const earlier = getRawDataOfCustomers().usage(customerID, laterYear - 1, month);
     return {laterAmount: later, change: later -earlier};    // 前年同月比較
 }
 
@@ -49,6 +49,10 @@ class customerData {
 
     get rawData() {
         return _.cloneDeep(this._data);
+    }
+
+    usage(customerID, year, month) {
+        return this._data[customerID].usages[year][month];
     }
 }
 

59b6144c1a10d75069a2e6c33387fa9abd33bf36
Soh1121 <satou.shg@gmail.com>
2024-04-01 08:11:52 +0900

    [add] サンプルコードを作成

---
+++ Remove/RemoveMiddleMan.js
@@ -0,0 +1,12 @@
+// client code...
+manager = aPerson.manager;
+
+class Person
+{
+    get manager() {return this._department.manager;}
+}
+
+class Department
+{
+    get manager() {return this._manager;}
+}

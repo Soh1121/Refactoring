92f6e917630a4d99e5d2bdbed70838ff71b2ab64
Soh1121 <satou.shg@gmail.com>
2024-03-31 18:23:21 +0900

    [refactor] データを読み書きするための関数を定義

--- Basic/EncapsulateVariable.js
+++ Basic/EncapsulateVariable.js
@@ -2,3 +2,6 @@ let defaultOwner = {firstName: "Martin", lastName: "Fowler"};
 
 spaceship.owner = defaultOwner;
 defaultOwner = {firstName: "Rebecca", lastName: "Parsons"};
+
+function getDefaultOwner() {return defaultOwner;}
+function setDefaultOwner(arg) {defaultOwner = arg;}

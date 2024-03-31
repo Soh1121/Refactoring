650bfc60f67cbbad5763526dc4fd06c8df5590f2
Soh1121 <satou.shg@gmail.com>
2024-03-31 18:30:04 +0900

    [update] サンプルコードを変更

--- Basic/EncapsulateVariable.js
+++ Basic/EncapsulateVariable.js
@@ -1,6 +1,12 @@
 spaceship.owner = getDefaultOwner();
 setDefaultOwner({firstName: "Rebecca", lastName: "Parsons"});
 
+const owner1 = defaultOwner();
+assert.equal("Fowler", owner1.lastName, "when set");
+const owner2 = defaultOwner();
+owner2.lastName = "Parsons";
+assert.equal("Parsons", owner1.lastName, "after change owner2"); // これでいい？
+
 // defaultOwner.js...
 let defaultData = {firstName: "Martin", lastName: "Fowler"};
 export function defaultOwner() {return defaultData;}

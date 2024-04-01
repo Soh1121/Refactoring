1be676d02a230fb94c35e7422765bfdc264afd27
Soh1121 <satou.shg@gmail.com>
2024-04-02 08:07:37 +0900

    [add] サンプルコードを作成

---
+++ Encapsulation/ReplacePrimitiveWithObject.js
@@ -0,0 +1,12 @@
+class Order
+{
+    constructor(data) {
+        this.priority = data.priority;
+        // 初期化が続く
+    }
+}
+
+// client...
+highPriorityCount = orders.filter(0 => "high" === o.priority
+                                    || "rush" === o.priority)
+                            .length;

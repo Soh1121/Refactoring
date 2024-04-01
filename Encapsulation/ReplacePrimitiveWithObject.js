c7137a81e1181a669c7d53190df48b6ee1474b64
Soh1121 <satou.shg@gmail.com>
2024-04-02 08:08:54 +0900

    [refactor]【変数のカプセル化】値オブジェクト化したい変数をカプセル化

--- Encapsulation/ReplacePrimitiveWithObject.js
+++ Encapsulation/ReplacePrimitiveWithObject.js
@@ -4,9 +4,11 @@ class Order
         this.priority = data.priority;
         // 初期化が続く
     }
+    get priority() {return this._priority;}
+    set priority(aString) {this._priority = aString;}
 }
 
 // client...
-highPriorityCount = orders.filter(0 => "high" === o.priority
+highPriorityCount = orders.filter(o => "high" === o.priority
                                     || "rush" === o.priority)
                             .length;

83bc9141aaa5367aa3ed29637baee4502f4be271
Soh1121 <satou.shg@gmail.com>
2024-04-02 08:10:28 +0900

    [refactor]値クラスを作成

--- Encapsulation/ReplacePrimitiveWithObject.js
+++ Encapsulation/ReplacePrimitiveWithObject.js
@@ -8,6 +8,12 @@ class Order
     set priority(aString) {this._priority = aString;}
 }
 
+class Priority
+{
+    constructor(value) {this._value = value;}
+    toString() {return this._value;}
+}
+
 // client...
 highPriorityCount = orders.filter(o => "high" === o.priority
                                     || "rush" === o.priority)

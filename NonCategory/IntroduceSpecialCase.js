cbb902837e1e29615cc22f73eb216dc607738070
Soh1121 <satou.shg@gmail.com>
2024-04-05 08:08:37 +0900

    [update] サンプルコードを変更

--- NonCategory/IntroduceSpecialCase.js
+++ NonCategory/IntroduceSpecialCase.js
@@ -1,6 +1,6 @@
 class Site
 {
-    get customer() {return (this._customer === "unknown") ? new UnknownCustomer() : this._customer;}
+    get customer() {return this._customer;}
 }
 
 class Customer
@@ -9,19 +9,6 @@ class Customer
     get billingPlan() {}
     set billingPlan(arg) {}
     get paymentHistory() {}
-    get isUnknown() {return false;}
-}
-
-class UnknownCustomer {
-    get name() {return "occupant";}
-    get billingPlan() {return registry.billingPlans.basic;}
-    set billingPlan(arg) { /* 何もしない */ }
-    get paymentHistory() {return new NullPaymentHistorymentHistory();}
-    get isUnknown() {return true;}
-}
-
-class NullPaymentHistory {
-    get weeksDelinquentInLastYear() {return 0;}
 }
 
 // client 1...
@@ -29,29 +16,23 @@ function client1()
 {
     const aCustomer = site.customer;
     // ... 大量のコードが入る ...
-    let customerName = aCustomer.name;
+    let customerName;
+    if (aCustomer === "unknown") customerName = "occupant";
+    else customerName = aCustomer.name;
 }
 
 // client 2...
 function client2()
 {
-    const plan = aCustomer.billingPlan;
+    const plan = (aCustomer === "unknown") ?
+        registry.billingPlans.basic
+        : aCustomer.billingPlan;
 }
 
 // client 3...
 function client3()
 {
-    aCustomer.billingPlan = newPlan;
-}
-
-// client 4...
-function client4()
-{
-    const weeksDelinquent = aCustomer.paymentHistory.weeksDelinquentInLastYear;
-}
-
-// client...
-function client()
-{
-    const name = aCustomer.isUnknown ? "unknown occupant" : aCustomer.name;
+    const weeksDelinquent = (aCustomer === "unknown") ?
+        0
+        : aCustomer.paymentHistory.weeksDelinquentInLastYear;
 }

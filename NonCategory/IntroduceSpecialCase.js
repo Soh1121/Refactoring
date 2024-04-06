960597937e49fcfdc4ef323c71f9cf0c902ea85f
Soh1121 <satou.shg@gmail.com>
2024-04-06 22:46:44 +0900

    [refactor] 道の顧客かどうか判定する箇所を関数に変更

--- NonCategory/IntroduceSpecialCase.js
+++ NonCategory/IntroduceSpecialCase.js
@@ -28,14 +28,14 @@ function client1()
     const aCustomer = site.customer;
     // ... 大量のコードが入る ...
     let customerName;
-    if (aCustomer === "unknown") customerName = "occupant";
+    if (isUnknown(aCustomer)) customerName = "occupant";
     else customerName = aCustomer.name;
 }
 
 // client 2...
 function client2()
 {
-    const plan = (aCustomer === "unknown") ?
+    const plan = (isUnknown(aCustomer)) ?
         registry.billingPlans.basic
         : aCustomer.billingPlan;
 }
@@ -43,13 +43,13 @@ function client2()
 // client 3...
 function client3()
 {
-    if (aCustomer === "unknown") aCustomer.billingPlan = newPlan;
+    if (isUnknown(aCustomer)) aCustomer.billingPlan = newPlan;
 }
 
 // client 4...
 function client4()
 {
-    const weeksDelinquent = (aCustomer === "unknown") ?
+    const weeksDelinquent = (isUnknown(aCustomer)) ?
         0
         : aCustomer.paymentHistory.weeksDelinquentInLastYear;
 }

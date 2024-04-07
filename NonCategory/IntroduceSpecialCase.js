ebb6e44007e826d69e767024d750f07b3a1fcbfa
Soh1121 <satou.shg@gmail.com>
2024-04-07 15:34:31 +0900

    [refactor]【関数の抽出】特殊ケースの判定処理

--- NonCategory/IntroduceSpecialCase.js
+++ NonCategory/IntroduceSpecialCase.js
@@ -18,20 +18,24 @@ function createUnknownCustomer() {
     };
 }
 
+function isUnknown(arg) {
+    return (arg === 'unknown');
+}
+
 // client 1...
 function client1()
 {
     const aCustomer = site.customer;
     // ... 大量のコードが入る ...
     let customerName;
-    if (aCustomer === "unknown") customerName = "occupant";
+    if (isUnknown(aCustomer)) customerName = "occupant";
     else customerName = aCustomer.name;
 }
 
 // client 2...
 function client2()
 {
-    const plan = (aCustomer === "unknown") ?
+    const plan = (isUnknown(aCustomer)) ?
         registry.billingPlans.basic
         : aCustomer.billingPlan;
 }
@@ -39,7 +43,7 @@ function client2()
 // client 3...
 function client3()
 {
-    const weeksDelinquent = (aCustomer === "unknown") ?
+    const weeksDelinquent = (isUnknown(aCustomer)) ?
         0
         : aCustomer.paymentHistory.weeksDelinquentInLastYear;
 }
